df78740a258143b7e91abe5f0e499f570f797f06
Kratagya-Webkorps <166798407+Kratagya-Webkorps@users.noreply.github.com>
2024-05-02 15:46:40 +0530

    Add files via upload

--- addBlog.js
+++ addBlog.js
@@ -1,70 +1,9 @@
-let addYourPost = document.getElementById("addYourBlog")
-let cardBlock = document.getElementById("cardBlock")
-let confirmBlogSubmit = document.getElementById("confirmBlogSubmit")
-let cancelBlogSubmit = document.getElementById("cancelBlogSubmit")
-let addNewBlog = document.getElementById("addNewBlog")
-let exploreBlock1 = document.getElementById("exploreBlock")
-let signOut = document.getElementById("signOut")
 
-signOut.addEventListener("click",()=>{
+signOut.addEventListener("click", () => {
+    localStorage.removeItem("loginDetails")
     window.location.href = "index.html"
-
 })
-
-
-
-const addBlogFromJson = async () => {
-    let userBlogs = await fetch('./blogDetails.json')
-    let response = await userBlogs.json()
-    console.log( typeof response.users[0].tags)
-    response.users.map((e) => {
-        console.log(e)
-        createCard(e.name, e.title, e.body, e.tags)
-
-    })
-}
-addBlogFromJson()
-
-
-
-
-addYourPost.addEventListener("click", function () {
-    exploreBlock.style.display = "none";
-    cardBlock.style.display = "none";
-    addNewBlog.style.display = "block";
-})
-
-cancelBlogSubmit.addEventListener("click", function () {
-    exploreBlock.style.display = "block";
-    addNewBlog.style.display = "none";
-    cardBlock.style.display = "block";
-
-})
-
-confirmBlogSubmit.addEventListener("click", function () {
-    let pEmail = document.getElementById("userEmailId").value
-    let pName = document.getElementById("publisherName").value
-    let pTitle = document.getElementById("publisherTitle").value
-    let pBody = document.getElementById("publisherBody").value
-    let pTags = document.getElementById("publisherTags").value
-
-    let getData = { pEmail, pName, pTitle, pBody, pTags }
-    console.log(getData)
-    // pushDataIntoJson(pEmail, pName, pTitle, pBody, pTags)
-
-    createCard( pName, pTitle, pBody, pTags)
-    document.getElementById('userEmailId').value = '';
-    document.getElementById('publisherName').value = '';
-    document.getElementById('publisherTitle').value = '';
-    document.getElementById('publisherBody').value = '';
-    document.getElementById('publisherTags').value = '';
-    exploreBlock.style.display = "block";
-    addNewBlog.style.display = "none";
-    cardBlock.style.display = "block";
-})
-
-const createCard = (...args) => {
-
+const createCard = (name, title, body, tags, likes, key) => {
     let newCard = document.createElement('div');
     let cardBody = document.createElement('div');
     let cardText = document.createElement('p');
@@ -72,33 +11,146 @@ const createCard = (...args) => {
     let cardTitle = document.createElement('h5');
     let cardTags = document.createElement('p');
     let deleteBtn = document.createElement('button');
-    let cardBlock1 = document.getElementById("cardBlock1")
+    let likeBtn = document.createElement('button');
+    let commentBtn = document.createElement('button');
+    let cardBlock1 = document.getElementById("cardBlock1");
+
 
     newCard.className = 'card mx-5 mt-4';
     newCard.style.width = '18rem';
 
     cardBody.className = 'card-body';
-    cardTitle.id = "card-title"
+    cardTitle.id = "card-title";
     cardTitle.className = 'card-title';
-    cardTitle.textContent = `${args[1]}`;
+    cardTitle.textContent = `${title}`;
 
     cardSubtitle.className = 'card-subtitle mb-2 text-muted';
     cardSubtitle.id = 'card-subtitle';
-    cardSubtitle.textContent = `${args[2]}`;
+    cardSubtitle.textContent = `${body}`;
 
     cardText.className = 'card-text';
     cardText.id = 'card-text';
-    cardText.textContent = `${args[3]}`;
+    cardText.textContent = `${tags}`;
 
     cardTags.className = 'card-text';
     cardTags.id = 'card-link';
-    cardTags.textContent = `${args[0]}`;
+    cardTags.textContent = `${name}`;
 
-    deleteBtn.id = "deleteBlog"
-    deleteBtn.className = "btn btn-sm btn-danger mx-2"
-    deleteBtn.textContent = "delete"
+    likeBtn.className = "btn btn-sm btn-danger mx-2 myClass";
+    likeBtn.id = "likeBlog";
+    likeBtn.innerHTML = `&#9829; ${likes}`;
+    likeBtn.addEventListener("click", () => {
+        let storedData = localStorage.getItem('blogDetails');
+        let parsedData = JSON.parse(storedData);
+        parsedData.users[key].likes++;
+        let updatedData = JSON.stringify(parsedData);
+        localStorage.setItem('blogDetails', updatedData);
+        likeBtn.innerHTML = `&#9829; ${parsedData.users[key].likes}`;
+    });
+
+    const cloneCard = (originalCard) => {
+        let clonedCard = originalCard.cloneNode(true);
+        return clonedCard;
+    };
+
+    commentBtn.className = "btn btn-sm btn-danger mx-2 far fa-comment";
+    commentBtn.id = "commentBlog";
+    commentBtn.textContent = 'comment';
+    commentBtn.addEventListener("click", function () {
+        let parentCard = this.parentNode.parentNode;
+        let clonedCard = cloneCard(parentCard);
+        let commentSection = document.createElement('div');
+        let commentArea = document.createElement('div');
+        let commentInput = document.createElement('textarea');
+        let submitCommentBtn = document.createElement('button');
+        let closeCommentBtn = document.createElement('button');
+        commentArea.id = "commentArea"
+        commentArea.style.display = "flex"
+        commentSection.id = "commentSection"
+        commentSection.className = 'comment-section input-group mt-3';
+
+        commentInput.className = 'comment-input';
+        commentSection.style.height = "50px"
+        commentSection.style.display = "flex"
+        commentInput.placeholder = 'Write your comment here...';
+
+        submitCommentBtn.className = 'btn btn-primary submit-comment-btn';
+        submitCommentBtn.textContent = 'Submit';
+        closeCommentBtn.className = 'btn btn-dark submit-comment-btn';
+        closeCommentBtn.textContent = 'Close';
+        submitCommentBtn.addEventListener('click', function () {
+            let commentText = commentInput.value;
+            console.log(commentText);
+            let storedData = localStorage.getItem('blogDetails');
+            let parsedData = JSON.parse(storedData);
+            let parsedLogin = JSON.parse(localStorage.getItem('loginDetails'));
+            console.log(parsedData.users[key].comment)
+            let newComment = { viewerName: parsedLogin[0].name, comments: [commentText] }
+            parsedData.users[key].comment.push(newComment);/////////////////check
+            let updatedData = JSON.stringify(parsedData);
+            localStorage.setItem('blogDetails', updatedData);
+            commentInput.value = '';
+            while (commentContent.firstChild) {
+                commentContent.removeChild(commentContent.firstChild);
+            }
+            parsedData.users[key].comment.forEach(comment => {
+                let singleComment = document.createElement("div");
+                singleComment.id = "oldComments";
+                singleComment.innerHTML = `Name:- ${comment.viewerName} and Comment:- ${comment.comments}`;
+                commentContent.appendChild(singleComment);
+            });
+        });
+
+
+        closeCommentBtn.addEventListener("click", function () {
+            commentArea.remove();
+            cardBlock1.style.display = "flex";
+        })
+        commentSection.appendChild(commentInput);
+        commentSection.appendChild(submitCommentBtn);
+        commentSection.appendChild(closeCommentBtn);
+
+        let storedData = localStorage.getItem('blogDetails');
+        let parsedData = JSON.parse(storedData);
+        let comments = parsedData.users[key].comment;
+
+        let commentsDiv = document.createElement("div")
+        let commentHeading = document.createElement("h2")
+        commentHeading.innerHTML = "Comments"
+
+        let commentContent = document.createElement("div")
+        commentContent.id = "commentContent"
+
+        comments.forEach(comment => {//check
+            console.log(comment)
+            let singleComment = document.createElement("div");
+            singleComment.id = "oldComments"
+            singleComment.innerHTML = `Name:- ${comment.viewerName} and Comment:- ${comment.comments}`;
+            commentContent.appendChild(singleComment);
+        });
+
+        commentArea.appendChild(clonedCard);
+        commentsDiv.appendChild(commentHeading);
+        commentsDiv.appendChild(commentContent);
+        commentsDiv.appendChild(commentSection);
+        commentArea.appendChild(commentsDiv);
+
+        landingPage.append(commentArea);
+        cardBlock1.style.display = "none";
+    });
+    deleteBtn.id = "deleteBlog";
+    deleteBtn.className = "btn btn-sm btn-danger mx-1";
+    deleteBtn.textContent = "delete";
     deleteBtn.addEventListener("click", function () {
+        let storedData = localStorage.getItem('blogDetails');
+        let parsedData = JSON.parse(storedData);
+        console.log(parsedData)
         let deleteCard = this.closest("div.card");
+        let cardIndex = Array.from(deleteCard.parentElement.children).indexOf(deleteCard);
+        parsedData.users.splice(cardIndex, 1);
+
+        console.log(parsedData)
+        localStorage.setItem('blogDetails', JSON.stringify(parsedData));
         deleteCard.style.display = "none";
     });
 
@@ -106,16 +158,100 @@ const createCard = (...args) => {
     cardBody.appendChild(cardSubtitle);
     cardBody.appendChild(cardText);
     cardBody.appendChild(cardTags);
+    cardBody.appendChild(likeBtn);
+    cardBody.appendChild(commentBtn);
     if (window.location.href !== "http://127.0.0.1:5500/user.html") {
-        cardBody.appendChild(deleteBtn)
+        cardBody.appendChild(deleteBtn);
     }
 
     newCard.appendChild(cardBody);
-    cardBlock1.appendChild(newCard)
-}
+    cardBlock1.appendChild(newCard);
+};
 
 
-// const pushDataIntoJson = (...args) => {
+const addBlogFromJson = async () => {
+    let userBlogs = await fetch('./blogDetails.json');
+    let response = await userBlogs.json();
+    let dataExists = localStorage.getItem('blogDetails') !== null;
+
+    if (!dataExists) {
+        let blogDetails = response;
+        let jsonData = JSON.stringify(blogDetails);
+        localStorage.setItem('blogDetails', jsonData);
+    }
+
+    let storedData = localStorage.getItem('blogDetails');
+    const parsedData = JSON.parse(storedData);
+    let storedInJson = parsedData.users;
+    let key = 0;
+
+    storedInJson.forEach((e) => {
+        createCard(e.name, e.title, e.body, e.tags, e.likes, key);
+        key++;
+    });
+};
+
+addBlogFromJson();
 
 
-// }
+document.getElementById("addYourBlog").addEventListener("click", function () {
+    exploreBlock.style.display = "none";
+    cardBlock.style.display = "none";
+    addNewBlog.style.display = "block";
+    if (document.getElementById("commentArea")) {
+        commentArea.style.display = "none";
+    }
+});
+
+cancelBlogSubmit.addEventListener("click", function () {
+    document.getElementById('publisherEmailId').value = '';
+    document.getElementById('publisherName').value = '';
+    document.getElementById('publisherTitle').value = '';
+    document.getElementById('publisherBody').value = '';
+    document.getElementById('publisherTags').value = '';
+    exploreBlock.style.display = "block";
+    addNewBlog.style.display = "none";
+    cardBlock.style.display = "block";
+    if (document.getElementById("commentArea")) {
+        commentArea.style.display = "flex";
+    }
+});
+
+confirmBlogSubmit.addEventListener("click", function () {
+    let pEmail = document.getElementById("publisherEmailId").value;
+    let pName = document.getElementById("publisherName").value;
+    let pTitle = document.getElementById("publisherTitle").value;
+    let pBody = document.getElementById("publisherBody").value;
+    let pTags = document.getElementById("publisherTags").value;
+
+    let getData = {
+        "email": pEmail, "name": pName, "title": pTitle, "body": pBody, "tags": pTags, "likes": 0, "comment": {
+            "viewerName": "",
+            "comments": []
+        }
+    };
+    let updatedData = localStorage.getItem('blogDetails');
+    const parsedData = JSON.parse(updatedData);
+    let storedInJson = parsedData.users;
+    storedInJson.push(getData);
+    localStorage.setItem('blogDetails', JSON.stringify(parsedData));
+    document.getElementById('publisherEmailId').value = '';
+    document.getElementById('publisherName').value = '';
+    document.getElementById('publisherTitle').value = '';
+    document.getElementById('publisherBody').value = '';
+    document.getElementById('publisherTags').value = '';
+    exploreBlock.style.display = "block";
+    addNewBlog.style.display = "none";
+    cardBlock.style.display = "flex";
+
+    let parentElement = document.getElementById('cardBlock1');
+    while (parentElement.firstChild) {
+        parentElement.removeChild(parentElement.firstChild);
+    }
+
+    let key = 0;
+    storedInJson.forEach((e) => {
+        createCard(e.name, e.title, e.body, e.tags, e.likes, key);
+        key++;
+    });
+});
--- index.js
+++ index.js
@@ -1,49 +1,139 @@
 let submitLoginPage = document.getElementById("submitLoginPage")
+let signinPage = document.getElementById("signinPage")
 let loginPage = document.getElementById("loginPage")
 let landingPage = document.getElementById("landingPage")
 let exploreBlock = document.getElementById("exploreBlock")
 let exampleInputEmail1 = document.getElementById("exampleInputEmail1")
 let exampleInputPassword1 = document.getElementById("exampleInputPassword1")
-
-
-submitLoginPage.addEventListener("click", async function () {
-    let result = await checkIfAdmin()
-    console.log(result)
-    if (result === 1) {
-        window.location.href = "admin.html"
-    }
-    else if(result === 2) {
-        window.location.href = "user.html"
-        console.log("Enter valid details admin")
-    }
-    
-
+let signinPageBlock = document.getElementById("signinPageBlock")
+let cancelSignin = document.getElementById("cancelSignin")
+let confirmSignin = document.getElementById("confirmSignin")
+let clearLocalData = document.getElementById("clearLocalData")
+clearLocalData.addEventListener("click", function () {
+    localStorage.clear()
 })
-const checkIfAdmin = async () => {
+
+
+
+window.onload = async function () {
     let myData = await fetch('./data.json')
     let response = await myData.json()
-    return check(response)
+    let dataExists = localStorage.getItem('addLoginDetails') !== null;
+
+    if (!dataExists) {
+        let blogDetails = response;
+        let jsonData = JSON.stringify(blogDetails);
+        localStorage.setItem('addLoginDetails', jsonData);
+    }
+
+}
+confirmSignin.addEventListener("click", function () {
+
+    let newUserEmailId = document.getElementById("newUserEmailId")
+    let newUserPassword1 = document.getElementById("newUserPassword1")
+    let newUserName = document.getElementById("newUserName")
+    if (newUserEmailId.value !== "" && newUserPassword1.value !== "" && newUserName.value !== "") {
+        let storedData = localStorage.getItem('addLoginDetails');
+        console.log(newUserEmailId)
+        const parsedData = JSON.parse(storedData);
+        let storedInJson = parsedData;
+        console.log(storedInJson[0].otherUsers)
+
+
+        let newUser = {
+            "email": `${newUserEmailId.value}`,
+            "password": `${newUserPassword1.value}`,
+            "name": `${newUserName.value}`
+        };
+        storedInJson[0].otherUsers.push(newUser);
+        localStorage.setItem('addLoginDetails', JSON.stringify(parsedData));
+        console.log(storedInJson)
+        loginPage.style.display = "block"
+        signinPageBlock.style.display = "none"
+        document.getElementById('newUserEmailId').value = '';
+        document.getElementById('newUserPassword1').value = '';
+        document.getElementById('newUserName').value = '';
+    }
+    else {
+        let signinDetails = document.getElementById("signinDetails")
+        let errorMsg = document.createElement("p")
+        errorMsg.innerHTML = "Please Enter Valid Details"
+        signinDetails.appendChild(errorMsg)
+    }
+})
+if (submitLoginPage) {
+    submitLoginPage.addEventListener("click", async function () {
+        let result = await checkIfAdmin()
+        if (result === 1) {
+            window.location.href = "admin.html"
+
+        }
+        else if (result === 2) {
+            window.location.href = "user.html"
+            console.log("Enter valid details admin")
+        }
+
+
+
+    })
+}
+
+const checkIfAdmin = async () => {
+    let storedData = localStorage.getItem('addLoginDetails');
+    const parsedData = JSON.parse(storedData);
+    return check(parsedData)
 };
 
-const check = (response) => {
+const check = (storedInJson) => {
     let inputEmail = exampleInputEmail1.value
     let inputPass = exampleInputPassword1.value
+    let dataExists = localStorage.getItem('loginDetails') !== null;
+    if (!dataExists) {
+        localStorage.setItem('loginDetails', JSON.stringify([]));
+    }
+
     let flag = 0
-    if (inputEmail === response[0].admin.email && inputPass === response[0].admin.password) {
+    if (inputEmail === storedInJson[0].admin.email && inputPass === storedInJson[0].admin.password) {
+        let defaultLoginDetails = [{ "name": storedInJson[0].admin.name, "email": storedInJson[0].admin.email }];
+        let jsonData = JSON.stringify(defaultLoginDetails);
+        localStorage.setItem('loginDetails', jsonData);
         console.log("Welcome admin")
         flag = 1
+        console.log("admin")
         return flag
     }
-    var otherUsers = response[0]["otherUsers"];
+    var otherUsers = storedInJson[0]["otherUsers"];
     for (var j = 0; j < otherUsers.length; j++) {
         var user = otherUsers[j];
-        if (user.email === inputEmail) {
+
+        if (user.email === inputEmail && user.password === inputPass) {
+            let defaultLoginDetails = [{ "name": user.name, "email": user.email }];
+            let jsonData = JSON.stringify(defaultLoginDetails);
+            localStorage.setItem('loginDetails', jsonData);
             console.log("first")
-            flag =2
+            flag = 2
             return flag;
         }
     }
 }
+
+signinPage.addEventListener("click", function () {
+
+    loginPage.style.display = "none"
+    signinPageBlock.style.display = "block"
+
+})
+cancelSignin.addEventListener("click", function () {
+
+    loginPage.style.display = "block"
+    signinPageBlock.style.display = "none"
+
+})
+
+
+
+
+
 
 
 
